c7c33ffc77d11ed01bf67071255d014a8b58615a
FilipGrela <56220954+FilipGrela@users.noreply.github.com>
2021-11-24 17:49:45 +0100

    Add TCP communication :cloud:

--- Client/main.py
+++ Client/main.py
@@ -1,19 +1,24 @@
 import pygame
 import time
+import json
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
 
 class bcolors:
     HEADER = '\033[95m' # magenta
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
-    WARNING = '\033[93m' + '[WARNING] '
+    WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
 def init_controller():
-    print(bcolors.BOLD + "Initialize Joystick" + bcolors.ENDC)
+    print(bcolors.BOLD + "Initialize Joystick..." + bcolors.ENDC)
 
     pygame.joystick.quit()
     pygame.joystick.init()
@@ -26,7 +31,7 @@ def init_controller():
 
     elif num_of_devides > 1:
         joysticks = [pygame.joystick.Joystick(x) for x in range(num_of_devides)]
-        print(bcolors.OKCYAN + "[INFO]" + bcolors.ENDC + f" {len(joysticks)} joysticks detected, select one:")
+        print(bcolors.OKCYAN + "[INFO] " + bcolors.ENDC + f" {len(joysticks)}joysticks detected, select one:")
         joystick_num = 1
 
         for joystick in joysticks:
@@ -35,19 +40,19 @@ def init_controller():
         
         try:
             selected_joystick = int(input())-1
-            while(selected_joystick > len(joysticks) - 1 or selected_joystick <= 0):
-                if selected_joystick > len(joysticks)-1 or selected_joystick <= 0:
-                    print(bcolors.FAIL + "[ERROR]" + bcolors.ENDC + f" Joystick number \"{selected_joystick + 1}\" out of range.")
+            while(selected_joystick > len(joysticks) - 1 or selected_joystick < 0):
+                if selected_joystick > len(joysticks)-1 or selected_joystick < 0:
+                    print(bcolors.FAIL + "[ERROR] " + bcolors.ENDC + f"Joystick number \"{selected_joystick + 1}\" is out of range.")
                 selected_joystick = int(input())-1
             
             joystick = pygame.joystick.Joystick(selected_joystick)
 
         except ValueError:
-            print(bcolors.FAIL + "[ERROR]" + bcolors.ENDC + " This is not a joystick number.")
+            print(bcolors.FAIL + "[ERROR] " + bcolors.ENDC + "This is not a joystick number.")
             init_controller()
         
     elif num_of_devides == 0: 
-        print(bcolors.WARNING + "No joysticks available!" + bcolors.ENDC + "\nWaiting 5 seconds for joystick.")
+        print(bcolors.WARNING + "[WARNING] " + "No joysticks available!" + bcolors.ENDC + "\nWaiting 5 seconds for joystick.")
         time.sleep(5)
         init_controller()
 
@@ -57,8 +62,17 @@ def init_controller():
 
 def main():
     print(bcolors.ENDC)
-    joystick = init_controller()
+    # joystick = init_controller()
+    
+    example_json = json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]')
+    send_json_data(example_json)
 
+def send_json_data(msg):
+    send_data(json.dumps(msg, sort_keys=True, separators=(',', ':')))
+
+def send_data(msg):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(msg.encode('utf-8'), (UDP_IP, UDP_PORT))
 
 if __name__ == "__main__":
     main()
